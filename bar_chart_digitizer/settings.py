4170302c115d49a18dbaa51cc83b58b265189618
sushant <nsushant021@gmail.com>
2025-06-06 11:35:58 -0600

    Updated settings

--- bar_chart_digitizer/settings.py
+++ bar_chart_digitizer/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2v@q3u3!ropg2#b74deg$t3l*5_=pk^%p@_de#r!9yotet2q=)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG= False
 
 ALLOWED_HOSTS = []
 
