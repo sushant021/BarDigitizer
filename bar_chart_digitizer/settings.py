3bedcb620b7f20e24ba772bb0051be794f91355c
sushant <nsushant021@gmail.com>
2025-06-06 11:36:57 -0600

    Updated settings

--- bar_chart_digitizer/settings.py
+++ bar_chart_digitizer/settings.py
@@ -124,8 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 # # Static files (CSS, JavaScript, Images)
-# STATIC_URL = '/static/'
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'digitizer/static')]
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'digitizer/static')]
 
 # Media files
 MEDIA_URL = '/media/'
